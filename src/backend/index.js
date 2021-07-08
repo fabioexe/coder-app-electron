1e444900bf11f9c7c68334511de55946b4fea65f
Fabio Antunes <fabio.avasc@gmail.com>
2021-07-08 17:51:27 -0300

    Captura das palavras do arquivo .srt com maps, filters e regex

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,13 +1,20 @@
 const { ipcMain } = require('electron')
+const pathsToRows = require('./pathsToRows')
+const prepareData = require('./prepareData')
 
 ipcMain.on('process-subtitles', (event, paths) => {
     console.log(paths)
 
-    event.reply('process-subtitles', [
-        {name:'i', amount: 1253},
-        {name:'you', amount: 900},
-        {name:'he', amount: 853},
-        {name:'she', amount: 1253},
-        {name:'there', amount: 900},
-    ])
+    pathsToRows(paths)
+        .then(rows => prepareData(rows))
+        .then(words => console.log(words))
+        .then(() => {
+            event.reply('process-subtitles', [
+                {name:'i', amount: 1253},
+                {name:'you', amount: 900},
+                {name:'he', amount: 853},
+                {name:'she', amount: 1253},
+                {name:'there', amount: 900},
+            ])
+        })
 })
