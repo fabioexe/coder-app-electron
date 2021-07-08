d469927cdb9cfdd077c6029bdce05cf5c8c19544
Fabio Antunes <fabio.avasc@gmail.com>
2021-07-08 10:57:21 -0300

    Criação do layout

---
+++ src/components/Home.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-container fluid>
+      <v-form>
+          <v-file-input 
+            label="Selecione as legendas"
+            prepend-icon="mdi-message-text"
+            append-outer-icon="mdi-send"
+            outlined
+            multiple
+            chips
+            v-model="files"
+            @click:append-outer="processSubtitles" />
+      </v-form>
+      <div class="pills">
+          <Pill v-for="word in groupedWords" :key="word.name"
+          :name="word.name" :amount="word.amount"/>
+
+      </div>
+  </v-container>
+</template>
+
+<script>
+
+import Pill from './Pill'
+
+export default {
+    components: { Pill },
+    data(){
+        return {
+            groupedWords: [
+                {name:'i', amount: 1253},
+                {name:'you', amount: 900},
+                {name:'he', amount: 853},
+                {name:'i', amount: 1253},
+                {name:'you', amount: 900},
+                {name:'he', amount: 853},
+                {name:'i', amount: 1253},
+                {name:'you', amount: 900},
+                {name:'he', amount: 853},
+                {name:'i', amount: 1253},
+                {name:'you', amount: 900},
+                {name:'he', amount: 853},
+                {name:'i', amount: 1253},
+                {name:'you', amount: 900},
+                {name:'he', amount: 853},
+            ]
+        }
+    },
+    methods: {
+        processSubtitles(){
+            console.log(this.files)
+        }
+    }
+}
+</script>
+
+<style>
+
+.pills {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+}
+
+</style>
